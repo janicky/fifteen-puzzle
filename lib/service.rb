b8ede5f4bb01a30a34ad1d43936ec67412a9cb6f
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-06 19:59:23 +0100

    Service variables name correction

--- lib/service.rb
+++ lib/service.rb
@@ -4,7 +4,7 @@ require_relative "report"
 class Service
   def initialize(params)
     # Assign params
-    vars = %w[algorithm acronym input_file results_file stats_file]
+    vars = %w[algorithm acronym input_file solution_file stats_file]
     vars.each_with_index do |value, index|
       instance_variable_set("@#{value}".to_sym, params[index])
     end
